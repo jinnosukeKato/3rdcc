aad8c5d9ba0cffebe3a6abf8aed3995c17db49a4
KATO Jinnosuke <jinnosukeKato@gmail.com>
2025-09-06 14:58:47 +0900

    加減算を実装

--- 3rdcc.c
+++ 3rdcc.c
@@ -25,10 +25,31 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   put_asm_global(".intel_syntax noprefix");
   put_asm_global(".globl main");
   put_asm_global("main:");
 
-  put_asm("mov rax, %d", atoi(argv[1]));
+  put_asm("mov rax, %ld", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      put_asm("add rax, %ld", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      put_asm("sub rax, %ld", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   put_asm("ret");
+  return 0;
 }
