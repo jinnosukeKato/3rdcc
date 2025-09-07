e6ebd5ff459759a467797499ee93a10a930900a8
KATO Jinnosuke <jinnosukeKato@gmail.com>
2025-09-07 11:52:15 +0900

    トークナイザを導入

--- 3rdcc.c
+++ 3rdcc.c
@@ -1,6 +1,9 @@
+#include <ctype.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void error(char *fmt, ...) {
   va_list ap;
@@ -8,6 +11,86 @@ void error(char *fmt, ...) {
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
+}
+
+typedef enum {
+  TK_RESERVED,  // 記号
+  TK_NUM,       // 整数
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;  // トークン自身の文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) return false;
+
+  token = token->next;
+  return true;
+}
+
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c'ではありません", op);
+
+  token = token->next;
+}
+
+int expect_number() {
+  if (token->kind != TK_NUM) error("数値ではありません");
+
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() { return token->kind == TK_EOF; }
+
+// 新しいトークンを作成し，curにつなげる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできない入力: %s", p);
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
 }
 
 void put_asm_global(const char *fmt, ...) {
@@ -31,29 +114,23 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  token = tokenize(argv[1]);
 
   put_asm_global(".intel_syntax noprefix");
   put_asm_global(".globl main");
   put_asm_global("main:");
 
-  put_asm("mov rax, %ld", strtol(p, &p, 10));
+  // 式の最初は数でなければならない
+  put_asm("mov rax, %d", expect_number());
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      put_asm("add rax, %ld", strtol(p, &p, 10));
+  while (!at_eof()) {
+    if (consume('+')) {
+      put_asm("add rax, %d", expect_number());
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      put_asm("sub rax, %ld", strtol(p, &p, 10));
-      continue;
-    }
-
-    error("予期しない文字です: '%c'", *p);
-    return 1;
+    expect('-');
+    put_asm("sub rax, %d", expect_number());
   }
 
   put_asm("ret");
