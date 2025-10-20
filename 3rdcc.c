41340151cdceb39dda697566fdec86fb250b908a
KATO Jinnosuke <jinnosukeKato@gmail.com>
2025-10-20 09:54:20 +0900

    括弧付き4則演算のできる言語へ進化

--- 3rdcc.c
+++ 3rdcc.c
@@ -46,6 +46,23 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) return false;
 
@@ -70,6 +87,109 @@ int expect_number() {
 
 bool at_eof() { return token->kind == TK_EOF; }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+  Node *node = mul();
+
+  while (1) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  while (1) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
+}
+
+void put_asm_global(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vprintf(fmt, args);
+  printf("\n");
+}
+
+void put_asm(const char *fmt, ...) {
+  printf("  ");
+  va_list args;
+  va_start(args, fmt);
+  vprintf(fmt, args);
+  printf("\n");
+}
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    put_asm("push %d", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  put_asm("pop rdi");
+  put_asm("pop rax");
+
+  switch (node->kind) {
+    case ND_ADD:
+      put_asm("add rax, rdi");
+      break;
+    case ND_SUB:
+      put_asm("sub rax, rdi");
+      break;
+    case ND_MUL:
+      put_asm("imul rax, rdi");
+      break;
+    case ND_DIV:
+      put_asm("cqo");
+      put_asm("idiv rdi");
+      break;
+  }
+
+  put_asm("push rax");
+}
+
 // 新しいトークンを作成し，curにつなげる
 Token *new_token(TokenKind kind, Token *cur, char *str) {
   Token *tok = calloc(1, sizeof(Token));
@@ -90,7 +210,8 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+        *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -108,21 +229,6 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
-void put_asm_global(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  vprintf(fmt, args);
-  printf("\n");
-}
-
-void put_asm(const char *fmt, ...) {
-  printf("  ");
-  va_list args;
-  va_start(args, fmt);
-  vprintf(fmt, args);
-  printf("\n");
-}
-
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません");
@@ -131,24 +237,15 @@ int main(int argc, char **argv) {
 
   user_input = argv[1];
   token = tokenize(argv[1]);
+  Node *node = expr();
 
   put_asm_global(".intel_syntax noprefix");
   put_asm_global(".globl main");
   put_asm_global("main:");
 
-  // 式の最初は数でなければならない
-  put_asm("mov rax, %d", expect_number());
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      put_asm("add rax, %d", expect_number());
-      continue;
-    }
-
-    expect('-');
-    put_asm("sub rax, %d", expect_number());
-  }
-
+  put_asm("pop rax");
   put_asm("ret");
   return 0;
 }
--- test.sh
+++ test.sh
@@ -20,5 +20,11 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 41 " 12 + 34  - 5 "
+assert 24 "3 * 8"
+assert 123 "5 * 20+23"
+assert 9 "81/9"
+assert 5 "28/5"
+assert 10 "2 +8 / 1"
+assert 47 "(1+3)*12-1"
 
 echo OK
